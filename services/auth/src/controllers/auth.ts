6b9ea2eddd2565bae4d5e5cb1c35f45fd550f3cb
ima-69 <imansha.idr@gmail.com>
2026-02-10 21:49:19 +0530

    feat(auth): enhance user registration with phone number, role validation, and JWT token generation

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -3,6 +3,8 @@ import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from "bcrypt";
+import axios from "axios";
+import jwt from "jsonwebtoken";
 
 export const registerUser = TryCatch(async (req, res, next) => {
   const { name, email, password, phoneNumber, role, bio } = req.body;
@@ -19,6 +21,13 @@ export const registerUser = TryCatch(async (req, res, next) => {
 
   if (existingUser.length > 0) {
     throw new ErrorHandler(400, "User with this email already exists");
+  }
+
+  if (role !== "recruiter" && role !== "jobseeker") {
+    throw new ErrorHandler(
+      400,
+      "Role must be either 'recruiter' or 'jobseeker'",
+    );
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -42,16 +51,32 @@ export const registerUser = TryCatch(async (req, res, next) => {
     const fileBuffer = getBuffer(file);
 
     if (!fileBuffer || !fileBuffer.content) {
-      throw new ErrorHandler(400, "Failed to generate buffer from uploaded file");
-    } 
+      throw new ErrorHandler(500, "Failed to generate buffer");
+    }
+
+    const { data } = await axios.post(
+      `${process.env.UPLOAD_SERVICE_URL}/api/utils/upload`,
+      { buffer: fileBuffer.content },
+    );
 
     const [user] =
-      await sql`INSERT INTO users (name, email, password, phone_number, role) VALUES 
-        (${name}, ${email}, ${hashedPassword}, ${phoneNumber}, ${role}) RETURNING 
-        user_id, name, email, phone_number, role, bio, created_at`;
+      await sql`INSERT INTO users (name, email, password, phone_number, role, bio, resume, resume_public_id) VALUES 
+        (${name}, ${email}, ${hashedPassword}, ${phoneNumber}, ${role}, ${bio}, ${data.url}, ${data.public_id}) RETURNING 
+        user_id, name, email, phone_number, role, bio, resume, created_at`;
 
     registeredUser = user;
   }
 
-  res.json(email);
+  const token = jwt.sign(
+    { id: registeredUser?.user_id },
+    process.env.JWT_SECRET as string, {
+      expiresIn: "15d",
+    }
+  );
+
+  res.status(201).json({
+    message: "User registered successfully",
+    registeredUser,
+    token,
+  });
 });
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -22,6 +22,7 @@ async function initDb() {
             name VARCHAR(255) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL,
+            phone_number VARCHAR(20) NOT NULL,
             role user_role NOT NULL,
             bio TEXT,
             resume VARCHAR(255),
