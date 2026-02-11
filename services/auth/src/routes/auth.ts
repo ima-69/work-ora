8341efdbfa045a088b2eb6900fcad5b9d4395c6f
ima-69 <imansha.idr@gmail.com>
2026-02-11 11:35:20 +0530

    update

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import {loginUser, registerUser} from "../controllers/auth.js";
-import uploadFile from '../middleware/multer.js';
+import express from "express";
+import { loginUser, registerUser } from "../controllers/auth.js";
+import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.post("/register", uploadFile, registerUser)
-router.post("/login", loginUser)
+router.post("/register", uploadFile, registerUser);
+router.post("/login", loginUser);
 
 export default router;
